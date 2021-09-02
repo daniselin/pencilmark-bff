cc8bc87a0e5624933a66fbe84d14ef0047ac8f4c
Dan Iselin <you@example.com>
2021-09-02 13:52:12 -0500

    change baseURL

--- frontend/src/axiosApi.js
+++ frontend/src/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://127.0.0.1:8000/api/'
+const baseURL = 'http://https://pencilmark2.herokuapp.com/'
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
