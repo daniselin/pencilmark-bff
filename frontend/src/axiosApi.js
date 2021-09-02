b5140d78f8bda03f73bf5392c8ca8e8b98979df1
Dan Iselin <you@example.com>
2021-09-02 14:44:47 -0500

    get csrftoken cookie

--- frontend/src/axiosApi.js
+++ frontend/src/axiosApi.js
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
         'Authorization': localStorage.getItem('access_token') ? "JWT " + localStorage.getItem('access_token') : null,
         'Content-Type': 'application/json',
         'accept': 'application/json',
-        "X-CSRFToken": document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        "X-CSRFToken": getCookie('csrftoken')
     }
 });
 
@@ -69,5 +69,21 @@ axiosInstance.interceptors.response.use(
       return Promise.reject(error);
   }
 );
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 export default axiosInstance
